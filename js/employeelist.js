ec3cd4b18c926d3f1b98570dc820c4909c199312
satyamchinn4u <satyamchinna4u@gmail.com>
2015-04-01 10:28:56 +0530

    hrms
    hrms project

---
+++ js/employeelist.js
@@ -0,0 +1,22 @@
+var serviceURL = "http://hrms.ctrls.com/mobile/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+
+function getEmployeeList() {
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="http://hrms.ctrls.com/profilepics/' + employee.picture + '"/>' +
+					'<h4>' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
+					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
